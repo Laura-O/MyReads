c1d1c4cb6170c65e722b0ee358348f730d30b31f
Laura-O <laura.ohrndorf@gmail.com>
2017-07-04 02:16:08 +0200

    Add modal with additional information

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import Image from 'react-bootstrap/lib/Image';
+import Image from 'react-bootstrap/lib/Image'
+import Modal from 'react-bootstrap/lib/Modal'
+import Button from 'react-bootstrap/lib/Button'
 
-class Book extends Component {       
+class Book extends Component {
+  state = {
+    showModal: false
+  }
+  
+  closeModal = () => {
+    this.setState({showModal: false})
+  }
+
+  showModal = () => {
+    this.setState({showModal: true})
+  }
+
   render() {
     const {book, onChangeStatus} = this.props
+    let description = book.description
+    let categories = book.categories
+    let pageCount = book.pageCount
+    let authors = book.authors
     
     return (
       <div className='book'>
         <div className='book-top'>
           <div className='book-cover'>
-            <Image src={book.imageLinks.smallThumbnail} responsive thumbnail />
+            <div className="book-cover" onClick={this.showModal} style={{ width: 128, height: 188, backgroundImage: `url('${book.imageLinks.smallThumbnail}')` }}>
+           <div className="overlay">
+             <div className="info"></div>
+           </div>
+         </div>
           </div>        
           <div className='book-shelf-changer'>
              <select defaultValue={book.shelf} onChange={(event) => onChangeStatus(event, book)}>
@@ -24,7 +46,28 @@ class Book extends Component {
          </div>
           <div className='book-title'>{book.title}</div>
           <div className='book-authors'>{book.authors ? book.authors[0] : ''}</div>
+          
+          <Modal show={this.state.showModal} onHide={this.closeModal} bsSize="lg">
+          <Modal.Header closeButton>
+              <Modal.Title>{book.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body bsClass="modal-body">
+            <div>
+              <p><strong>Authors</strong>: {authors}</p>
+              <p><strong>Category</strong>: {categories}</p>              
+              <p><strong>Pages:</strong>: {pageCount}</p>
+              <hr />
+              <h4>Description</h4>
+              <p>{description}</p>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.closeModal}>Close</Button>
+          </Modal.Footer>
+        </Modal>
       </div>
+      
+      
       )
   }
 }
